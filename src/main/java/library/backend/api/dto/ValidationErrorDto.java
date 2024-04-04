71960fc95d177f9447a537282001d117af79db38
jsq1234 <mananprakash3078@gmail.com>
2024-04-04 10:42:04 +0530

    removed issues from user model

--- src/main/java/library/backend/api/dto/ValidationErrorDto.java
+++ src/main/java/library/backend/api/dto/ValidationErrorDto.java
@@ -1,11 +1,8 @@
 package library.backend.api.dto;
 
-import library.backend.api.utils.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 import org.springframework.http.HttpStatus;
 
--- src/main/java/library/backend/api/models/User.java
+++ src/main/java/library/backend/api/models/User.java
@@ -2,10 +2,8 @@ package library.backend.api.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
